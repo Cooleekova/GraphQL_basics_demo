104898f982d4faef0e835101b6654574fad738cb
cooleekova <nushnushka@gmail.com>
2022-09-09 12:08:56 -0300

    Create graphql mutation instance for adding new books and authors into database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express()
 const authors = [
 	{ id: 1, name: 'J. K. Rowling' },
 	{ id: 2, name: 'J. R. R. Tolkien' },
-	{ id: 3, name: 'Brent Weeks' }
+	{ id: 3, name: 'Brent Weeks' },
 ]
 
 const books = [
@@ -25,7 +25,7 @@ const books = [
 	{ id: 5, name: 'The Two Towers', authorId: 2 },
 	{ id: 6, name: 'The Return of the King', authorId: 2 },
 	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 },
 ]
 
 const BookType = new GraphQLObjectType({
@@ -90,12 +90,45 @@ const RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(AuthorType),
             description: 'List of All Authors',
             resolve: () => authors
-        }
+        },
+    })
+})
+
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: 'Add a book',
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+                books.push(book)
+                return book
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            description: 'Add an author',
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve: (parent, args) => {
+                const author = { id: authors.length + 1, name: args.name }
+                authors.push(author)
+                return author
+            }
+        },
     })
 })
 
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 app.use('/graphql', graphqlHTTP({
@@ -103,5 +136,7 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 app.listen(5000., () => console.log('Server Running'))
+
+
 
 
